6a23cb3f86873f5455cb7676d48af4ae7644f2ed
AlphaRacer <tarun.a1441@gmail.com>
2021-02-09 18:41:49 +0530

    Make autoresponder a function

--- index.js
+++ index.js
@@ -130,74 +130,8 @@ client.on('message', (message) => {
     }
 
     // Auto-responder
-    const userinput = message.content.toLowerCase();
-    const db1 = {
-        set1: {
-            triggers: [
-                'hi',
-                'hey',
-                'hello',
-                'heyya',
-                'hewwo',
-                'sup',
-                'wassup',
-                'hoi',
-                'hai',
-                'yo',
-            ],
-            replies: ['Hello there!', 'Hi!', 'Hey!', "What's up?", 'Sup!'],
-        },
-        set2: {
-            triggers: ['bye', 'cya', 'gtg'],
-            replies: ['Bye!', 'See you later!'],
-        },
-        set3: {
-            triggers: [
-                'good',
-                'great',
-                'nice',
-                'noice',
-                'yay',
-                'awesome',
-                'cool',
-            ],
-            replies: ['Awesome!', 'Great!'],
-        },
-        set4: {
-            triggers: ['lol', 'lmao', 'haha', 'xd'],
-            replies: [
-                '<:ar_themadman:690898831901982730>',
-                ':rofl:',
-                ':joy:',
-                'LOL',
-                'Haha!',
-            ],
-        },
-    };
-
-    // If the message content contains any greeting words
-    if (db1.set1.triggers.includes(userinput)) {
-        result = Math.floor(Math.random() * db1.set1.replies.length);
-        message.channel.send(db1.set1.replies[result]);
-    }
-
-    // If the message content contains any words like bye
-    if (db1.set2.triggers.includes(userinput)) {
-        result = Math.floor(Math.random() * db1.set2.replies.length);
-        message.channel.send(db1.set2.replies[result]);
-    }
-
-    // If the message content contains any words like thank you
-    if (db1.set3.triggers.includes(userinput)) {
-        result = Math.floor(Math.random() * db1.set3.replies.length);
-        message.channel.send(db1.set3.replies[result]);
-    }
-
-    // If the message content contains any words like thank you
-    if (db1.set4.triggers.includes(userinput)) {
-        result = Math.floor(Math.random() * db1.set4.replies.length);
-        message.channel.send(db1.set4.replies[result]);
-    }
+    const { Autoresponder } = require('./modules/Autoresponder');
+    Autoresponder();
 
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/);
@@ -206,7 +140,6 @@ client.on('message', (message) => {
     if (!client.commands.has(command)) return;
 
     try {
-        // if (message.author.id != '568066367518015506') return;
         client.commands.get(command).execute(message, args);
     } catch (error) {
         console.error(error);
